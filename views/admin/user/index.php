3b1b04bb12a20314af1e000f922f365b1d977b4d
dergus <dergesus@gmail.com>
2015-07-13 11:20:24 +0300

    fixed username attribute to name in gridview widget

--- views/admin/user/index.php
+++ views/admin/user/index.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'username',
+            'name',
             'about:ntext',
              'birthdate',
             ['attribute'=>'sex',

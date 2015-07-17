cdc552a5bbcc1e5eacc78a5b70982fc253c83d0a
dergus <dergesus@gmail.com>
2015-07-17 19:34:06 +0300

    added forum ctrl and views and moved forum index action from category ctrl to site ctrl

---
+++ views/site/_view.php
@@ -0,0 +1,15 @@
+<?php use yii\helpers\Html;?>
+<div class="row">
+    <div class="col-xs-12">
+    <table class="table">
+        <tr style="background: #3aab81"> <th colspan="3"><?=Html::a($model->title,['category/view','id'=>$model->id],['style'=>"color:black"]);?></th></tr>
+        <div class="row">
+        <?php foreach ($model->forums as $forum):?>
+        
+            <tr><td class="col-sm-10"><?=Html::a($forum->title,['forum/view','id'=>$forum->id]);?><div class="text-muted"><?= $forum->description?></div></td><td class="col-sm-2"><?=$forum->count?> Theme</td></tr>
+        <?php endforeach;?>
+        </div>
+    </table>
+    </div>
+</div>
+
--- views/site/index.php
+++ views/site/index.php
@@ -1,51 +1,28 @@
 <?php
+
+use yii\helpers\Html;
+use yii\widgets\ListView;
+
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Categories');
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-index">
+<div class="category-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+    <div class="row">
+        <div class="col-xs-10  col-md-10 col-md-offset-1">
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'layout'=>'{items}',
+        'itemView' =>'_view'
+        
+    ]) ?>
         </div>
-
     </div>
+
 </div>

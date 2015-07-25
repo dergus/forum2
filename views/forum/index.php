f1ef6e0857204242e47f3901ddf9fc3d061f48bf
dergus <dergesus@gmail.com>
2015-07-25 22:39:18 +0300

    added id to link for creating themes

--- views/forum/index.php
+++ views/forum/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $forum->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Create Theme'), ['theme/create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Create Theme'), ['theme/create','id'=>$forum->id], ['class' => 'btn btn-success']) ?>
     </p>
 <?php if($fixedThemes->totalCount!==0): ?>
 <div class="row">
